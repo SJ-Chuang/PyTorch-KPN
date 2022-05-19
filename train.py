a6ed2a2791ba58ba988e3f03c8a509cc500d3102
SJ-Chuang <r08521606@ntu.edu.tw>
2022-05-19 23:22:41 +0800

    update training example

--- train.py
+++ train.py
@@ -10,34 +10,39 @@ class DataList:
     def __init__(self, image_path):
         self.image_path = image_path
         
-    def get_datalist(self):
+    def __call__(self):
         data_list = []
-        for p in self.image_path:
-            h, w = cv2.imread(p).shape[:2]
-            pre, ext = os.path.splitext(p)
-            label = json.load(open(pre+".json"))
-            data_list.append({
-                "image": p,
-                "height": h,
-                "width": w,
-                "annotations": [
-                    {
-                        "keypoint": np.mean(shape["points"], 0),
-                        "category_id": 0
-                    } for shape in label["shapes"]
-                ]
+        global max_id
+        for file in os.listdir(self.image_path):
+            name, ext = os.path.splitext(file)
+            if ext == ".json":
+                label = json.load(open(os.path.join(self.image_path, file)))
+                h, w = cv2.imread(os.path.join(self.image_path, name+".jpg")).shape[:2]
+                annos = []
+                for idx, (x, y, visable) in enumerate(label["hand_pts"]):
+                    if visable:
+                        annos.append({
+                            "keypoint": [x, y],
+                            "category_id": idx
+                        })
+                        
+                data_list.append({
+                    "image": os.path.join(self.image_path, name+".jpg"),
+                    "height": h,
+                    "width": w,
+                    "annotations": annos
+                })
                 
-                
-            })
         return data_list
 
-DatasetCatalog.register("train", DataList(open("../train.txt").read().splitlines()).get_datalist)
-DatasetCatalog.register("val", DataList(open("../val.txt").read().splitlines()).get_datalist)
+DatasetCatalog.register("train", DataList("hand_synth/train"))
+DatasetCatalog.register("val", DataList("hand_synth/val"))
 
 cfg = get_cfg()
 cfg.DATASETS.TRAIN = ("train",)
 cfg.DATASETS.VAL = ("val",)
 cfg.SOLVER.EPOCH = 300
+cfg.MODEL.NUM_CLASSES = 21
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 trainer = DefaultTrainer(cfg)
@@ -45,5 +50,4 @@ trainer.train()
 
 model = trainer.model
 model.load_state_dict(torch.load(os.path.join(cfg.OUTPUT_DIR, "best_kpn.pth")))
-
 do_evaluation(cfg, model, dataset_name="val")
